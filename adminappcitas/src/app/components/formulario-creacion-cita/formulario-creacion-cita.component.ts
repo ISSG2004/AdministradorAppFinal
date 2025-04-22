994a7259bb7359308fbcd468eb5b3a307066c3b5
ISSG2004 <ismaelsangon1234@gmail.com>
2025-04-22 19:58:26 +0200

    Anotaciones de como crear la cita

--- adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
+++ adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './formulario-creacion-cita.component.css'
 })
 export class FormularioCreacionCitaComponent {
-
+  //creando el objeto cita
+  //ponemos un calendario en el que seleccionamos fecha de inicio y fecha de fin del rango en el que vamos a crear las citas
+  //ponemos un item para seleccionar los horarios de apertura del negocio (ver como hacer para seleccionar el horario en jornada partida)
+  //Duración de cita (en minutos)
 }
