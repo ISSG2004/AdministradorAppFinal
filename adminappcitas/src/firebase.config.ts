9229cee074d48e8f7de724c7fa5193d718b85052
ISSG2004 <ismaelsangon1234@gmail.com>
2025-05-07 08:49:16 +0200

    Añadiendo nuevas dependencias de nuevo proyecto firebase

--- adminappcitas/src/firebase.config.ts
+++ adminappcitas/src/firebase.config.ts
@@ -1,10 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAYxsLO5lWFw_ryi3Y-MfIMyeQL8pmmgGM",
-  authDomain: "crudrealtimeclase.firebaseapp.com",
-  databaseURL: "https://crudrealtimeclase-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "crudrealtimeclase",
-  storageBucket: "crudrealtimeclase.firebasestorage.app",
-  messagingSenderId: "754438865373",
-  appId: "1:754438865373:web:d06569ef4d3b2a2f693634",
-  measurementId: "G-37GWWYHC8Q"
+  apiKey: "AIzaSyDPHBDFYyQ-6QRULG8lnMdQyxVZo_at2q8",
+  authDomain: "appo-8d144.firebaseapp.com",
+  databaseURL: "https://appo-8d144-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "appo-8d144",
+  storageBucket: "appo-8d144.firebasestorage.app",
+  messagingSenderId: "237782310317",
+  appId: "1:237782310317:web:920c263dd754df27c85524",
+  measurementId: "G-9W21S1H82Y"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- adminappcitas/src/main.ts
+++ adminappcitas/src/main.ts
@@ -5,12 +5,13 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { firebaseConfig } from './firebase.config';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore()),
+    provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({ projectId: "appo-8d144", appId: "1:237782310317:web:920c263dd754df27c85524", databaseURL: "https://appo-8d144-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "appo-8d144.firebasestorage.app", apiKey: "AIzaSyDPHBDFYyQ-6QRULG8lnMdQyxVZo_at2q8", authDomain: "appo-8d144.firebaseapp.com", messagingSenderId: "237782310317", measurementId: "G-9W21S1H82Y" })), provideAuth(() => getAuth()), provideDatabase(() => getDatabase()),
   ],
 }).catch(err => console.error(err));
 
