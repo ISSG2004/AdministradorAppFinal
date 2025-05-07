d4db76550a4c100c538877ab76928a7da87714ec
ISSG2004 <ismaelsangon1234@gmail.com>
2025-05-07 09:27:21 +0200

    Corrección configuración de app

--- adminappcitas/src/app/app.config.ts
+++ adminappcitas/src/app/app.config.ts
@@ -8,5 +8,18 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideFirebaseApp(() => initializeApp({ projectId: "crudrealtimeclase", appId: "1:754438865373:web:d06569ef4d3b2a2f693634", databaseURL: "https://crudrealtimeclase-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "crudrealtimeclase.firebasestorage.app", apiKey: "AIzaSyAYxsLO5lWFw_ryi3Y-MfIMyeQL8pmmgGM", authDomain: "crudrealtimeclase.firebaseapp.com", messagingSenderId: "754438865373", measurementId: "G-37GWWYHC8Q" })), provideAuth(() => getAuth()), provideDatabase(() => getDatabase())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(
+      {
+        projectId: "appo-8d144",
+        appId: "1:237782310317:web:920c263dd754df27c85524",
+        databaseURL: "https://appo-8d144-default-rtdb.europe-west1.firebasedatabase.app",
+        messagingSenderId: "237782310317",
+        measurementId: "G-9W21S1H82Y"
+      }
+    )),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())]
 };
