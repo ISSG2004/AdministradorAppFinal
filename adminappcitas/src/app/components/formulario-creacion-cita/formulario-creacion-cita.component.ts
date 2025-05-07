65732d8843ae7e47a6bef3982c8c7215e5c0aede
ISSG2004 <ismaelsangon1234@gmail.com>
2025-05-07 19:21:20 +0200

    Subiendo los cambios en el package.json

--- adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
+++ adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-formulario-creacion-cita',
   imports: [],
