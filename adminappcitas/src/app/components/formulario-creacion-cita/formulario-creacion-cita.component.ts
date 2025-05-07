35873abfab170f4300548f2a0c9b5d1a57d6dceb
ISSG2004 <ismaelsangon1234@gmail.com>
2025-05-07 21:57:50 +0200

    Creación del steeper y de los items de la parte dedicada a fechas

--- adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
+++ adminappcitas/src/app/components/formulario-creacion-cita/formulario-creacion-cita.component.ts
@@ -6,6 +6,9 @@ import {MatStepperModule} from '@angular/material/stepper';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatButtonModule} from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-formulario-creacion-cita',
   imports: [
@@ -16,16 +19,21 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatSelectModule,
+    MatOption,
+    CommonModule
   ],
   templateUrl: './formulario-creacion-cita.component.html',
   styleUrl: './formulario-creacion-cita.component.css'
 })
 export class FormularioCreacionCitaComponent {
+
   //variable para no pasar de step hasta que no se haya rellenado el formulario
   isLinear = true;
-  //variable para el formulario reactivo pt1
+  //variables para el formulario reactivo pt1
   primerFormulario!: FormGroup;
+  diasSemanaLista: any[] = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
   //variable para el formulario reactivo pt2
   segundoFormulario!:FormGroup;
   constructor(private formBuilder: FormBuilder) {}
@@ -39,6 +47,7 @@ export class FormularioCreacionCitaComponent {
       //añadimos los campos que vamos a añadir en este formulario
       fechaInicio: ['' , Validators.required],
       fechaFin: ['', Validators.required],
+      diasLibres: ['', Validators.required],
     });
     //Inicializamos el formulario reactivo pt2
     this.segundoFormulario = this.formBuilder.group({
